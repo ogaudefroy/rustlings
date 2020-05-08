b3304875b00482b07f2adafa05e673ad6aff14cd
Olivier GAUDEFROY <ogaudefroy@vente-privee.com>
2020-05-08 13:14:14 +0200

    Added some comments.

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,9 +2,13 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
+    solution1(); // Pass a clone of data instead --> data ain't borrowed anymore.
+    solution2(); // Pass a reference, clone data in function and return a new vector.
+    solution3(); // Pass a mutable reference and update vector content directly.
+}
+
+fn solution1() {
     let vec0 = Vec::new();
 
     let mut vec1 = fill_vec(vec0.clone());
@@ -17,6 +21,32 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
+fn solution2() {
+    let vec0 = Vec::new();
+
+    let mut vec1 = fill_vec_borrow(&vec0);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec1.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+}
+
+fn solution3() {
+    let mut vec0 = Vec::new();
+
+    fill_vec_mut_borrow(&mut vec0);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec0.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+}
+
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
@@ -25,4 +55,20 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec.push(66);
 
     vec
+}
+
+fn fill_vec_borrow(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.clone();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
+
+fn fill_vec_mut_borrow(vec: &mut Vec<i32>) {
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
 }
