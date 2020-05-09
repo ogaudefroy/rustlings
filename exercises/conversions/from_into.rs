3e7d6943ecdfc5d86e2d6a5e3acca596ebb25e80
Olivier GAUDEFROY <ogaudefroy@vente-privee.com>
2020-05-09 11:28:04 +0200

    From test.

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -18,7 +18,6 @@ impl Default for Person {
     }
 }
 
-// I AM NOT DONE
 // Your task is to complete this implementation
 // in order for the line `let p = Person::from("Mark,20")` to compile
 // Please note that you'll need to parse the age component into a `usize`
@@ -34,6 +33,22 @@ impl Default for Person {
 // Otherwise, then return an instantiated Person onject with the results
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person {
+                ..Default::default()
+            };
+        }
+        let mut splitted_string = s.split(',');
+        let name = splitted_string.next().unwrap();
+        let age = splitted_string.next().unwrap().parse::<usize>();
+
+        match age {
+            Ok(s) => Person {
+                name: name.to_string(),
+                age: s,
+            },
+            _ => Person::default(),
+        }
     }
 }
 
